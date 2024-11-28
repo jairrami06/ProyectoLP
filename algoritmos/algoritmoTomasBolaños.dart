3ef9424e582f21907cd0604896f0a6ada2175d69
Tomagochito <tbolanos@espol.edu.ec>
2024-11-28 01:03:27 -0500

    algoritmotomascambiado

--- algoritmos/algoritmoTomasBolaños.dart
+++ algoritmos/algoritmoTomasBolaños.dart
@@ -1,8 +1,18 @@
-int fibonacci(int n) {
-  if (n <= 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+void myfunction(){
+for (int i = 0; i < value; i++) {
+                   for (final candidate in candidates) {
+                        for (final Candidate(:atributo, :atributo) in candidates) {
+                             print('Hola mundo');
+                        }
+                   }
 }
 
-void main() {
-  print(fibonacci(8)); // 21
+print(4.5 * 2);
+var names = <String>{};
+int num1 = 203;
+double num2 = 305.3;
+double num3 = 34 + 5.3;
+double num4 = 23 * 10;
+double error = 3.4 + "string";
+num1 + num2;
 }
